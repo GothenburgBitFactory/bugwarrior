483c7eab1a69ee1c5c7a3def43b0e607767e4afd
Ralph Bean <rbean@redhat.com>
2012-10-06 12:06:24 -0400

    Futzing with dependency versions.

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setup(name='bugwarrior',
       install_requires=[
           "twiggy",
           "bitlyapi",
-          "pygithub3",
-          "requests==0.13.1",  # Temporary until pygithub3 can catch up.
+          "pygithub3<0.5",  # pygithub3==0.5 broke us.  :/
+          #"requests",  # Let pygithub3 dictate our python-requests version.
           "offtrac",
           "python-bugzilla",
           "taskw >= 0.4.2",
