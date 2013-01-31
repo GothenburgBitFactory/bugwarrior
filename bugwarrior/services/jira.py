64e18b7fb8dda2f4a57db697fb00c3a1849c2713
ubuntudroid <ubuntudroid@gmail.com>
2013-01-31 21:30:39 +0100

    Add default for jira.query
    Default query is the query from the old version and shall prevent
    silent fails for bugwarrior users running bugwarrior-pull as cron job
    or launchd task.

--- bugwarrior/services/jira.py
+++ bugwarrior/services/jira.py
@@ -30,7 +30,10 @@ class JiraService(IssueService):
         super(JiraService, self).__init__(*args, **kw)
         self.username = self.config.get(self.target, 'jira.username')
         self.url = self.config.get(self.target, 'jira.base_uri')
-        self.query = self.config.get(self.target, 'jira.query')
+        if self.config.has_option(self.target, 'jira.query'):
+        	self.query = self.config.get(self.target, 'jira.query')
+        else:
+			self.query = 'assignee=' + self.username + ' AND status != closed and status != resolved'
         self.jira = JIRA(options={'server': self.config.get(self.target, 'jira.base_uri')},
                          basic_auth=(self.username,
                                      self.config.get(self.target, 'jira.password')))
