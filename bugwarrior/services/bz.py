474e61eb8d2204855f6496b0bfa56f0e1aede3b4
Ralph Bean <rbean@redhat.com>
2013-11-09 09:09:28 -0500

    Try to support older bugzilla instances.

--- bugwarrior/services/bz.py
+++ bugwarrior/services/bz.py
@@ -3,7 +3,7 @@ from twiggy import log
 import bugzilla
 
 from bugwarrior.services import IssueService
-from bugwarrior.config import die, get_service_password
+from bugwarrior.config import die, asbool, get_service_password
 
 import datetime
 import time
@@ -42,6 +42,18 @@ class BugzillaService(IssueService):
         base_uri = self.config.get(self.target, 'bugzilla.base_uri')
         username = self.config.get(self.target, 'bugzilla.username')
         password = self.config.get(self.target, 'bugzilla.password')
+
+        # So more modern bugzilla's require that we specify
+        # query_format=advanced along with the xmlrpc request.
+        # https://bugzilla.redhat.com/show_bug.cgi?id=825370
+        # ...but older bugzilla's don't know anything about that argument.
+        # Here we make it possible for the user to specify whether they want
+        # to pass that argument or not.
+        self.advanced = True  # Default to True.
+        if self.config.has_option(self.target, 'bugzilla.advanced'):
+            self.advanced = asbool(self.config.get(
+                self.target, 'bugzilla.advanced'))
+
         if not password or password.startswith("@oracle:"):
             service = "bugzilla://%s@%s" % (username, base_uri)
             password = get_service_password(service, username, oracle=password,
@@ -99,10 +111,13 @@ class BugzillaService(IssueService):
             emailassigned_to1=1,
             emailqa_contact1=1,
             emailtype1="substring",
+        )
+
+        if self.advanced:
             # Required for new bugzilla
             # https://bugzilla.redhat.com/show_bug.cgi?id=825370
-            query_format='advanced',
-        )
+            query['query_format'] = 'advanced'
+
         bugs = self.bz.query(query)
 
         # Convert to dicts
