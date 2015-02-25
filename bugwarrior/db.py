b973e925bdda8da35b5090ff82212ba4a3a8190c
Ralph Bean <rbean@redhat.com>
2015-02-25 12:15:18 -0500

    Bugfix for legacy_matching.

--- bugwarrior/db.py
+++ bugwarrior/db.py
@@ -169,6 +169,9 @@ def find_local_uuid(tw, keys, issue, legacy_matching=True):
 
     if legacy_matching:
         legacy_description = issue.get_default_description().rsplit('..', 1)[0]
+        # Furthermore, we have to kill off any single quotes which break in
+        # task-2.4.x, as much as it saddens me.
+        legacy_description = legacy_description.split("'")[0]
         results = tw.filter_tasks({
             'description.startswith': legacy_description,
             'or': [
