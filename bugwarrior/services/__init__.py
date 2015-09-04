1667619bf8d099efbf4b8f509544ade28417254b
Ryan S. Brown <sb@ryansb.com>
2015-09-04 14:50:02 -0400

    Make comment annotation configurable
    This patch adds a general config option `annotation_comments` that
    defaults to True (current behavior) and lets users turn off annotations
    for issue comments. This is super handy if you're on large github
    issue threads.

--- bugwarrior/services/__init__.py
+++ bugwarrior/services/__init__.py
@@ -75,6 +75,12 @@ class IssueService(object):
                 config.get(self.main_section, 'annotation_links')
             )
 
+        self.annotation_comments = True
+        if config.has_option(self.main_section, 'annotation_comments'):
+            self.annotation_comments = asbool(
+                config.get(self.main_section, 'annotation_comments')
+            )
+
         self.shorten = False
         if config.has_option(self.main_section, 'shorten'):
             self.shorten = asbool(config.get(self.main_section, 'shorten'))
@@ -165,6 +171,8 @@ class IssueService(object):
         final = []
         if self.annotation_links:
             final.append(url)
+        if not self.annotation_comments:
+            return final
         for author, message in annotations:
             message = message.strip()
             if not message or not author:
