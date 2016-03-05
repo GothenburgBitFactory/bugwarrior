e2dfc826f4c6f6e1340df86d1ae4a17823d408d7
ryneeverett <ryneeverett@gmail.com>
2016-03-04 21:28:31 -0500

    Data store improvements.
    - Use lockfile for writes.
    - Use 'r+' rather than 'rw'. (The latter raises an exception in
      python 3).
    - Catch ValueError if data file isn't JSON.
    - Add `get_data_path` helper function.

--- bugwarrior/data.py
+++ bugwarrior/data.py
@@ -1,26 +1,35 @@
 import os
 import json
 
+from lockfile.pidlockfile import PIDLockFile
 
-DATAFILE = os.path.expanduser(
-    os.path.join(os.getenv('TASKDATA', '~/.task'), 'bugwarrior.data'))
+from bugwarrior.config import get_data_path
+
+
+DATAFILE = os.path.join(get_data_path(), 'bugwarrior.data')
+LOCKFILE = os.path.join(get_data_path(), 'bugwarrior-data.lockfile')
 
 
 def get(key):
     try:
         with open(DATAFILE, 'r') as jsondata:
-            data = json.load(jsondata)
-            return data[key]
+            try:
+                data = json.load(jsondata)
+            except ValueError:  # File does not contain JSON.
+                return None
+            else:
+                return data[key]
     except IOError:  # File does not exist.
         return None
 
 
 def set(key, value):
-    try:
-        with open(DATAFILE, 'rw') as jsondata:
-            data = json.load(jsondata)
-            data[key] = value
-            json.dump(data, jsondata)
-    except IOError:  # File does not exist.
-        with open(DATAFILE, 'w+') as jsondata:
-            json.dump({key: value}, jsondata)
+    with PIDLockFile(LOCKFILE):
+        try:
+            with open(DATAFILE, 'r+') as jsondata:
+                data = json.load(jsondata)
+                data[key] = value
+                json.dump(data, jsondata)
+        except IOError:  # File does not exist.
+            with open(DATAFILE, 'w+') as jsondata:
+                json.dump({key: value}, jsondata)
