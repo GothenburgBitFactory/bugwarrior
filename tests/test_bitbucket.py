6b3a8109fe1ac7f143a168faa72bea763bc70516
ryneeverett <ryneeverett@gmail.com>
2016-03-25 00:04:28 -0500

    Add responses and test Bitbucket issues.
    With responses as a test dependency we can test methods that make
    requests without hitting remote API's. This should help a lot with the
    test coverage of service modules.

--- tests/test_bitbucket.py
+++ tests/test_bitbucket.py
@@ -1,3 +1,5 @@
+import responses
+
 from bugwarrior.services.bitbucket import BitbucketService
 
 from .base import ServiceTest
@@ -43,3 +45,72 @@ class TestBitbucketIssue(ServiceTest):
         actual_output = issue.to_taskwarrior()
 
         self.assertEqual(actual_output, expected_output)
+
+    @responses.activate
+    def test_issues(self):
+        responses.add(
+            responses.GET,
+            'https://api.bitbucket.org/2.0/repositories/somename/',
+            json={'values': [{
+                'full_name': 'somename/somerepo',
+                'has_issues': True
+            }]})
+
+        responses.add(
+            responses.GET,
+            'https://api.bitbucket.org/2.0/repositories/somename/somerepo/issues/',
+            json={'values': [{
+                'title': 'Some Bug',
+                'status': 'open',
+                'links': {'html': {'href': 'example.com'}},
+                'id': 1
+            }]})
+
+        responses.add(
+            responses.GET,
+            'https://api.bitbucket.org/1.0/repositories/somename/somerepo/issues/1/comments',
+            json=[{
+                'author_info': {'username': 'nobody'},
+                'content': 'Some comment.'
+            }])
+
+        responses.add(
+            responses.GET,
+            'https://api.bitbucket.org/2.0/repositories/somename/somerepo/pullrequests/',
+            json={'values': [{
+                'title': 'Some Feature',
+                'state': 'open',
+                'links': {'html': {'href': 'example.com'}},
+                'id': 1
+            }]})
+
+        responses.add(
+            responses.GET,
+            'https://api.bitbucket.org/2.0/repositories/somename/somerepo/pullrequests/1/comments',
+            json={'values': [{
+                'user': {'username': 'nobody'},
+                'content': {'raw': 'Some comment.'}
+            }]})
+
+        issue, pr = [i for i in self.service.issues()]
+
+        self.assertEqual(issue['bitbuckettitle'], 'Some Bug')
+        self.assertEqual(
+            issue['description'], '(bw)Is#1 - Some Bug .. example.com')
+        self.assertEqual(issue['project'], 'somerepo')
+        self.assertEqual(issue['tags'], [])
+        self.assertEqual(issue['bitbucketurl'], 'example.com')
+        self.assertEqual(issue['priority'], 'M')
+        self.assertEqual(issue['bitbucketid'], 1)
+        self.assertEqual(issue['annotations'], ['@nobody - Some comment.'])
+
+        self.assertEqual(pr['bitbuckettitle'], 'Some Feature')
+        self.assertEqual(
+            pr['description'],
+            '(bw)Is#1 - Some Feature .. https://bitbucket.org/')
+        self.assertEqual(pr['project'], 'somerepo')
+        self.assertEqual(pr['tags'], [])
+        self.assertEqual(pr['bitbucketurl'], 'https://bitbucket.org/')
+        self.assertEqual(pr['priority'], 'M')
+        self.assertEqual(pr['bitbucketid'], 1)
+        self.assertEqual(pr['annotations'], ['@nobody - Some comment.'])
