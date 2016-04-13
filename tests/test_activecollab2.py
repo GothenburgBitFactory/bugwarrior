4084ab965de28de64e55512734c9d3ef00a64154
ryneeverett <ryneeverett@gmail.com>
2016-04-13 18:33:56 +0000

    Test activecollab2 issues and fix bugs.
    - self.target is referenced in ActiveCollab2Client but undefined.
    - "ticket" is not a valid type to pass to get_default_description

--- tests/test_activecollab2.py
+++ tests/test_activecollab2.py
@@ -1,6 +1,8 @@
+import re
 import datetime
 
 import pytz
+import responses
 
 from bugwarrior.services.activecollab2 import ActiveCollab2Service
 
@@ -9,52 +11,90 @@ from .base import ServiceTest
 
 class TestActiveCollab2Issue(ServiceTest):
     SERVICE_CONFIG = {
-        'activecollab2.url': 'hello',
+        'activecollab2.url': 'http://hello',
         'activecollab2.key': 'howdy',
-        'activecollab2.user_id': 'hola',
+        'activecollab2.user_id': 0,
         'activecollab2.projects': '1:one, 2:two'
+    }
+
+    arbitrary_due_on = (
+        datetime.datetime.now() - datetime.timedelta(hours=1)
+    ).replace(tzinfo=pytz.UTC)
+    arbitrary_created_on = (
+        datetime.datetime.now() - datetime.timedelta(hours=2)
+    ).replace(tzinfo=pytz.UTC)
+    arbitrary_issue = {
+        'project': 'something',
+        'priority': 2,
+        'due_on': arbitrary_due_on.isoformat(),
+        'permalink': 'http://wherever/',
+        'ticket_id': 10,
+        'project_id': 20,
+        'type': 'Ticket',
+        'created_on': arbitrary_created_on.isoformat(),
+        'created_by_id': '10',
+        'body': 'Ticket Body',
+        'name': 'Anonymous',
+        'assignees': [
+            {'user_id': SERVICE_CONFIG['activecollab2.user_id'],
+             'is_owner': True}
+        ],
+        'description': 'Further detail.',
     }
 
     def setUp(self):
         self.service = self.get_mock_service(ActiveCollab2Service)
 
     def test_to_taskwarrior(self):
-        arbitrary_due_on = (
-            datetime.datetime.now() - datetime.timedelta(hours=1)
-        ).replace(tzinfo=pytz.UTC)
-        arbitrary_created_on = (
-            datetime.datetime.now() - datetime.timedelta(hours=2)
-        ).replace(tzinfo=pytz.UTC)
-        arbitrary_issue = {
-            'project': 'something',
-            'priority': 2,
-            'due_on': arbitrary_due_on.isoformat(),
-            'permalink': 'http://wherever/',
-            'ticket_id': 10,
-            'project_id': 20,
-            'type': 'issue',
-            'created_on': arbitrary_created_on.isoformat(),
-            'created_by_id': '10',
-            'body': 'Ticket Body',
-            'name': 'Anonymous',
-        }
 
-        issue = self.service.get_issue_for_record(arbitrary_issue)
+        issue = self.service.get_issue_for_record(self.arbitrary_issue)
 
         expected_output = {
-            'project': arbitrary_issue['project'],
-            'priority': issue.PRIORITY_MAP[arbitrary_issue['priority']],
-            'due': arbitrary_due_on,
+            'project': self.arbitrary_issue['project'],
+            'priority': issue.PRIORITY_MAP[self.arbitrary_issue['priority']],
+            'due': self.arbitrary_due_on,
 
-            issue.PERMALINK: arbitrary_issue['permalink'],
-            issue.TICKET_ID: arbitrary_issue['ticket_id'],
-            issue.PROJECT_ID: arbitrary_issue['project_id'],
-            issue.TYPE: arbitrary_issue['type'],
-            issue.CREATED_ON: arbitrary_created_on,
-            issue.CREATED_BY_ID: arbitrary_issue['created_by_id'],
-            issue.BODY: arbitrary_issue['body'],
-            issue.NAME: arbitrary_issue['name'],
+            issue.PERMALINK: self.arbitrary_issue['permalink'],
+            issue.TICKET_ID: self.arbitrary_issue['ticket_id'],
+            issue.PROJECT_ID: self.arbitrary_issue['project_id'],
+            issue.TYPE: self.arbitrary_issue['type'],
+            issue.CREATED_ON: self.arbitrary_created_on,
+            issue.CREATED_BY_ID: self.arbitrary_issue['created_by_id'],
+            issue.BODY: self.arbitrary_issue['body'],
+            issue.NAME: self.arbitrary_issue['name'],
         }
         actual_output = issue.to_taskwarrior()
 
         self.assertEqual(actual_output, expected_output)
+
+    @responses.activate
+    def test_issues(self):
+        responses.add(
+            responses.GET,
+            re.compile(
+                'http://hello/\?token=howdy&path_info=%2Fprojects%2F[1-2]%2Fuser-tasks&format=json'),
+            match_querystring=True,
+            json=[self.arbitrary_issue])
+
+        responses.add(
+            responses.GET,
+            'http://hello/?token=howdy&path_info=%2Fprojects%2F20%2Ftickets%2F10&format=json',
+            match_querystring=True,
+            json=self.arbitrary_issue)
+
+        issue = next(self.service.issues())
+
+        self.assertEqual(issue['ac2name'], 'Anonymous')
+        self.assertEqual(issue['project'], 'something')
+        self.assertEqual(issue['ac2projectid'], 20)
+        self.assertEqual(issue['ac2createdon'], self.arbitrary_created_on)
+        self.assertEqual(issue['tags'], [])
+        self.assertEqual(issue['ac2body'], 'Ticket Body')
+        self.assertEqual(
+            issue['description'], '(bw)Is#10 - Anonymous .. http://wherever/')
+        self.assertEqual(issue['ac2createdbyid'], '10')
+        self.assertEqual(issue['due'], self.arbitrary_due_on)
+        self.assertEqual(issue['priority'], 'H')
+        self.assertEqual(issue['ac2permalink'], 'http://wherever/')
+        self.assertEqual(issue['ac2ticketid'], 10)
+        self.assertEqual(issue['ac2type'], 'Ticket')
