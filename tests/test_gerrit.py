d375a2aba21f6bb0ef2fb996212f10457ef8539b
ryneeverett <ryneeverett@gmail.com>
2016-04-30 14:23:40 +0000

    Test gerrit issues.

--- tests/test_gerrit.py
+++ tests/test_gerrit.py
@@ -1,24 +1,31 @@
-from bugwarrior.services.gerrit import GerritService
+import json
 
+import responses
+
+from bugwarrior.services.gerrit import GerritService
 from .base import ServiceTest
 
 
 class TestGerritIssue(ServiceTest):
     SERVICE_CONFIG = {
-        'gerrit.base_uri': 'one',
+        'gerrit.base_uri': 'https://one',
         'gerrit.username': 'two',
         'gerrit.password': 'three',
+    }
+
+    record = {
+        'project': 'nova',
+        '_number': 1,
+        'subject': 'this is a title',
+        'messages': [{'author': {'username': 'Iam Author'},
+                      'message': 'this is a message',
+                      '_revision_number': 1}],
     }
 
     def setUp(self):
         self.service = self.get_mock_service(GerritService)
 
     def test_to_taskwarrior(self):
-        record = {
-            'project': 'nova',
-            '_number': 1,
-            'subject': 'this is a title',
-        }
         extra = {
             'annotations': [
                 # TODO - test annotations?
@@ -26,7 +33,7 @@ class TestGerritIssue(ServiceTest):
             'url': 'this is a url',
         }
 
-        issue = self.service.get_issue_for_record(record, extra)
+        issue = self.service.get_issue_for_record(self.record, extra)
         actual = issue.to_taskwarrior()
         expected = {
             'annotations': [],
@@ -39,3 +46,27 @@ class TestGerritIssue(ServiceTest):
         }
 
         self.assertEqual(actual, expected)
+
+    @responses.activate
+    def test_issues(self):
+        responses.add(
+            responses.GET,
+            'https://one/a/changes/?q=is:open+is:reviewer&o=MESSAGES&o=DETAILED_ACCOUNTS',
+            match_querystring=True,
+            # The response has some ")]}'" garbage prefixed.
+            body=")]}'" + json.dumps([self.record]))
+
+        issue = next(self.service.issues())
+
+        expected = [
+            ('project', u'nova'),
+            ('description',
+             u'(bw)PR#1 - this is a title .. https://one/#/c/1/'),
+            ('tags', []),
+            ('gerritsummary', u'this is a title'),
+            ('gerriturl', 'https://one/#/c/1/'),
+            ('priority', 'M'),
+            ('gerritid', 1),
+            ('annotations', [u'@Iam Author - is is a message'])]
+
+        self.assertEqual(issue.items(), expected)
