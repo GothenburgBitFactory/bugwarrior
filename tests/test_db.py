6a3b34b2f753c2569a911f28fb07fb59084358d5
ryneeverett <ryneeverett@gmail.com>
2016-09-17 16:21:43 -0400

    More tests for db.merge_left.

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,19 +1,38 @@
 import unittest
 
 import taskw.task
-from bugwarrior.db import merge_left
+from bugwarrior import db
 
 
-class DBTest(unittest.TestCase):
+
+class TestMergeLeft(unittest.TestCase):
     def setUp(self):
         self.issue_dict = {'annotations': ['testing']}
 
-    def test_merge_left_with_dict(self):
-        task = {}
-        merge_left('annotations', task, self.issue_dict)
-        self.assertEquals(task, self.issue_dict)
+    def assertMerged(self, local, remote, **kwargs):
+        db.merge_left('annotations', local, remote, **kwargs)
+        self.assertEqual(local, remote)
 
-    def test_merge_left_with_taskw(self):
-        task = taskw.task.Task({})
-        merge_left('annotations', task, self.issue_dict)
-        self.assertEquals(task, self.issue_dict)
+    def test_with_dict(self):
+        self.assertMerged({}, self.issue_dict)
+
+    def test_with_taskw(self):
+        self.assertMerged(taskw.task.Task({}), self.issue_dict)
+
+    def test_already_in_sync(self):
+        self.assertMerged(self.issue_dict, self.issue_dict)
+
+    def test_rough_equality_hamming_false(self):
+        """ When hamming=False, rough equivalents are duplicated. """
+        remote = {'annotations': ['\n  testing  \n']}
+
+        db.merge_left('annotations', self.issue_dict, remote, hamming=False)
+        self.assertEqual(len(self.issue_dict['annotations']), 2)
+
+    def test_rough_equality_hamming_true(self):
+        """ When hamming=True, rough equivalents are not duplicated. """
+        remote = {'annotations': ['\n  testing  \n']}
+
+        db.merge_left('annotations', self.issue_dict, remote, hamming=True)
+        self.assertEqual(len(self.issue_dict['annotations']), 1)
+
