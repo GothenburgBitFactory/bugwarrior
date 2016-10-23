11b413807dfd239494e5d38d43d27c9242c238a5
ryneeverett <ryneeverett@gmail.com>
2016-10-23 10:26:40 -0400

    In python3, octals must begin with "0o".

--- tests/test_data.py
+++ tests/test_data.py
@@ -14,6 +14,11 @@ class TestData(ConfigTest):
         config.add_section('general')
         self.data = data.BugwarriorData(config, 'general')
 
+    def assert0600(self):
+        permissions = oct(os.stat(self.data.datafile).st_mode & 0o777)
+        # python2 -> 0600, python3 -> 0o600
+        self.assertIn(permissions, ['0600', '0o600'])
+
     def test_get_set(self):
         # "touch" data file.
         with open(self.data.datafile, 'w+') as handle:
@@ -24,12 +29,10 @@ class TestData(ConfigTest):
         self.assertEqual(self.data.get('key'), 'value')
         self.assertEqual(
             self.data.get_data(), {'old': 'stuff', 'key': 'value'})
-        self.assertEqual(
-            oct(os.stat(self.data.datafile).st_mode & 0777), '0600')
+        self.assert0600()
 
     def test_set_first_time(self):
         self.data.set('key', 'value')
 
         self.assertEqual(self.data.get('key'), 'value')
-        self.assertEqual(
-            oct(os.stat(self.data.datafile).st_mode & 0777), '0600')
+        self.assert0600()
