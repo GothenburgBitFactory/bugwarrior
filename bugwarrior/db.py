GothenburgBitFactory/bugwarrior

commit 0517a4a88f7cce8b323e3cf9156f75fd32887b39

Decode all byte strings from utf8 before any db actions.
Fixes #350.

bugwarrior/db.py
@@ -325,6 +325,20 @@ def synchronize(issue_generator, conf, main_section, dry_run=False):
     }
 
     for issue in issue_generator:
+
+        # We received this issue from The Internet, but we're not sure what
+        # kind of encoding the service providers may have handed us. Let's try
+        # and decode all byte strings from UTF8 off the bat.  If we encounter
+        # other encodings in the wild in the future, we can revise the handling
+        # here. https://github.com/ralphbean/bugwarrior/issues/350
+        for key in issue.keys():
+            if isinstance(issue[key], six.binary_type):
+                try:
+                    issue[key] = issue[key].decode('utf-8')
+                except UnicodeDecodeError:
+                    log.warn("Failed to interpret %r as utf-8" % key)
+
+
         try:
             existing_uuid = find_local_uuid(
                 tw, key_list, issue, legacy_matching=legacy_matching

commit 351fee08a690bf20dec5e7a5414e66d5158d00b9

Fix bug introduced in #458.

bugwarrior/db.py
@@ -326,24 +326,23 @@ def synchronize(issue_generator, conf, main_section, dry_run=False):
 
     for issue in issue_generator:
 
-        # We received this issue from The Internet, but we're not sure what
-        # kind of encoding the service providers may have handed us. Let's try
-        # and decode all byte strings from UTF8 off the bat.  If we encounter
-        # other encodings in the wild in the future, we can revise the handling
-        # here. https://github.com/ralphbean/bugwarrior/issues/350
-        for key in issue.keys():
-            if isinstance(issue[key], six.binary_type):
-                try:
-                    issue[key] = issue[key].decode('utf-8')
-                except UnicodeDecodeError:
-                    log.warn("Failed to interpret %r as utf-8" % key)
-
-
         try:
+            issue_dict = dict(issue)
+            # We received this issue from The Internet, but we're not sure what
+            # kind of encoding the service providers may have handed us. Let's try
+            # and decode all byte strings from UTF8 off the bat.  If we encounter
+            # other encodings in the wild in the future, we can revise the handling
+            # here. https://github.com/ralphbean/bugwarrior/issues/350
+            for key in issue_dict.keys():
+                if isinstance(issue_dict[key], six.binary_type):
+                    try:
+                        issue_dict[key] = issue_dict[key].decode('utf-8')
+                    except UnicodeDecodeError:
+                        log.warn("Failed to interpret %r as utf-8" % key)
+
             existing_uuid = find_local_uuid(
                 tw, key_list, issue, legacy_matching=legacy_matching
             )
-            issue_dict = dict(issue)
             _, task = tw.get_task(uuid=existing_uuid)
 
             # Drop static fields from the upstream issue.  We don't want to
@@ -374,7 +373,7 @@ def synchronize(issue_generator, conf, main_section, dry_run=False):
         except MultipleMatches as e:
             log.exception("Multiple matches: %s", six.text_type(e))
         except NotFound:
-            issue_updates['new'].append(dict(issue))
+            issue_updates['new'].append(issue_dict)
 
     notreally = ' (not really)' if dry_run else ''
     # Add new issues