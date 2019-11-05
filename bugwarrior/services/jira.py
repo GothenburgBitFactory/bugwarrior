2d543e58c76701a9a3fbff2be125e1d6f6ce9e86
Christian Kohlstedde <christian@kohlsted.de>
2019-11-05 10:53:25 +0100

    JIRA: Quote username to allow the @ sign

--- bugwarrior/services/jira.py
+++ bugwarrior/services/jira.py
@@ -266,8 +266,8 @@ class JiraService(IssueService):
         self.url = self.config.get('base_uri')
         password = self.get_password('password', self.username)
 
-        default_query = 'assignee=' + self.username + \
-            ' AND resolution is null'
+        default_query = 'assignee="' + self.username + \
+            '" AND resolution is null'
         self.query = self.config.get('query', default_query)
         if password == '@kerberos':
             auth = dict(kerberos=True)
