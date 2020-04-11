9c3cf84c163917fd1158a0a2d21480cfae9e66bb
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2020-04-11 13:56:51 +0200

    Use displayName for the author (Closes: #718)

--- bugwarrior/services/jira.py
+++ bugwarrior/services/jira.py
@@ -338,7 +338,7 @@ class JiraService(IssueService):
         comments = self.jira.comments(issue.key) or []
         return self.build_annotations(
             ((
-                comment.author.name,
+                comment.author.displayName,
                 comment.body
             ) for comment in comments),
             issue_obj.get_processed_url(issue_obj.get_url())
