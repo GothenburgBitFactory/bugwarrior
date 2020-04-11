50a2362aab1ffd5d9e25e22058137306b2ca7717
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2020-04-11 13:49:04 +0200

    Fix: SyntaxWarning: "is not" with a literal

--- bugwarrior/db.py
+++ bugwarrior/db.py
@@ -258,7 +258,7 @@ def run_hooks(conf, name):
         if pre_import is not None:
             for hook in pre_import:
                 exit_code = subprocess.call(hook, shell=True)
-                if exit_code is not 0:
+                if exit_code != 0:
                     msg = 'Non-zero exit code %d on hook %s' % (
                         exit_code, hook
                     )
