GothenburgBitFactory/bugwarrior

commit 1cbbcb68f715d809bc2f366e2a7b19e314057fe9

Use configuration values from pydantic model.
- Eliminates verbose (and tricky) configparser usage from most of
bugwarrior in favor of standard python object syntax.
- Uses default values from models rather than defining them inline where
they are likely to diverge from the documentation.

bugwarrior/db.py
@@ -4,7 +4,6 @@ standard_library.install_aliases()
 from builtins import zip
 from builtins import object
 
-from six.moves.configparser import NoOptionError, NoSectionError
 import json
 import os
 import re
@@ -17,7 +16,6 @@ import six
 from taskw import TaskWarriorShellout
 from taskw.exceptions import TaskwarriorError
 
-from bugwarrior.config.parse import asbool, aslist, get_taskrc_path
 from bugwarrior.notifications import send_notification
 
 import logging
@@ -301,29 +299,20 @@ def merge_left(field, local_task, remote_issue, hamming=False):
             new_count, field, len(local_task[field]),))
 
 
-def run_hooks(conf, name):
-    if conf.has_option('hooks', name):
-        pre_import = aslist(conf.get('hooks', name))
-        if pre_import is not None:
-            for hook in pre_import:
-                exit_code = subprocess.call(hook, shell=True)
-                if exit_code != 0:
-                    msg = 'Non-zero exit code %d on hook %s' % (
-                        exit_code, hook
-                    )
-                    log.error(msg)
-                    raise RuntimeError(msg)
+def run_hooks(pre_import):
+    for hook in pre_import:
+        exit_code = subprocess.call(hook, shell=True)
+        if exit_code != 0:
+            msg = 'Non-zero exit code %d on hook %s' % exit_code, hook
+            log.error(msg)
+            raise RuntimeError(msg)
 
 
 def synchronize(issue_generator, conf, main_section, dry_run=False):
-    def _bool_option(section, option, default):
-        try:
-            return asbool(conf.get(section, option))
-        except (NoSectionError, NoOptionError):
-            return default
+    main_config = conf[main_section]
 
-    targets = aslist(conf.get(main_section, 'targets'))
-    services = set([conf.get(target, 'service') for target in targets])
+    targets = main_config.targets.copy()
+    services = set([conf[target].service for target in targets])
     key_list = build_key_list(services)
     uda_list = build_uda_config_overrides(services)
 
@@ -334,28 +323,16 @@ def synchronize(issue_generator, conf, main_section, dry_run=False):
             'add to your taskrc file.'
         )
 
-    static_fields = ['priority']
-    if conf.has_option(main_section, 'static_fields'):
-        static_fields = aslist(conf.get(main_section, 'static_fields'))
-
     # Before running CRUD operations, call the pre_import hook(s).
-    run_hooks(conf, 'pre_import')
+    run_hooks(conf['hooks'].pre_import)
 
-    notify = _bool_option('notifications', 'notifications', False) and not dry_run
+    notify = conf['notifications'].notifications and not dry_run
 
     tw = TaskWarriorShellout(
-        config_filename=get_taskrc_path(conf, main_section),
+        config_filename=main_config.taskrc,
         config_overrides=uda_list,
         marshal=True,
     )
-
-    merge_annotations = _bool_option(main_section, 'merge_annotations', True)
-    merge_tags = _bool_option(main_section, 'merge_tags', True)
-    replace_tags = _bool_option(main_section, 'replace_tags', False)
-
-    static_tags = []
-    if conf.has_option(main_section, 'static_tags'):
-        static_tags = aslist(conf.get(main_section, 'static_tags'))
 
     issue_updates = {
         'new': [],
@@ -411,17 +388,17 @@ def synchronize(issue_generator, conf, main_section, dry_run=False):
 
             # Drop static fields from the upstream issue.  We don't want to
             # overwrite local changes to fields we declare static.
-            for field in static_fields:
+            for field in main_config.static_fields:
                 if field in issue_dict:
                     del issue_dict[field]
 
             # Merge annotations & tags from online into our task object
-            if merge_annotations:
+            if main_config.merge_annotations:
                 merge_left('annotations', task, issue_dict, hamming=True)
 
-            if merge_tags:
-                if replace_tags:
-                    replace_left('tags', task, issue_dict, static_tags)
+            if main_config.merge_tags:
+                if main_config.replace_tags:
+                    replace_left('tags', task, issue_dict, main_config.static_tags)
                 else:
                     merge_left('tags', task, issue_dict)
 
@@ -449,7 +426,7 @@ def synchronize(issue_generator, conf, main_section, dry_run=False):
         if dry_run:
             continue
         if notify:
-            send_notification(issue, 'Created', conf)
+            send_notification(issue, 'Created', conf['notifications'])
 
         try:
             new_task = tw.task_add(**issue)
@@ -491,7 +468,7 @@ def synchronize(issue_generator, conf, main_section, dry_run=False):
     succeeded_service_task_uuids = get_managed_task_uuids(
         tw,
         build_key_list(
-            set([conf.get(target, 'service') for target in targets])))
+            set([conf[target].service for target in targets])))
     issue_updates['closed'] = succeeded_service_task_uuids - seen_uuids
     log.info("Closing %i tasks", len(issue_updates['closed']))
     for issue in issue_updates['closed']:
@@ -506,7 +483,7 @@ def synchronize(issue_generator, conf, main_section, dry_run=False):
             continue
 
         if notify:
-            send_notification(task_info, 'Completed', conf)
+            send_notification(task_info, 'Completed', conf['notifications'])
 
         try:
             tw.task_done(uuid=issue)
@@ -515,8 +492,10 @@ def synchronize(issue_generator, conf, main_section, dry_run=False):
 
     # Send notifications
     if notify:
-        only_on_new_tasks = _bool_option('notifications', 'only_on_new_tasks', False)
-        if not only_on_new_tasks or len(issue_updates['new']) + len(issue_updates['changed']) + len(issue_updates['closed']) > 0:
+        updates = (len(issue_updates['new']) +
+                   len(issue_updates['changed']) +
+                   len(issue_updates['closed']))
+        if not main_config.notifications.only_on_new_tasks or updates > 0:
             send_notification(
                 dict(
                     description="New: %d, Changed: %d, Completed: %d" % (
@@ -526,7 +505,7 @@ def synchronize(issue_generator, conf, main_section, dry_run=False):
                     )
                 ),
                 'bw_finished',
-                conf,
+                conf['notificatons'],
             )
 
 
@@ -540,8 +519,8 @@ def build_key_list(targets):
 
 
 def get_defined_udas_as_strings(conf, main_section):
-    targets = aslist(conf.get(main_section, 'targets'))
-    services = set([conf.get(target, 'service') for target in targets])
+    targets = conf[main_section].targets
+    services = set([conf[target].service for target in targets])
     uda_list = build_uda_config_overrides(services)
 
     for uda in convert_override_args_to_taskrc_settings(uda_list):

bugwarrior/command.py
@@ -9,7 +9,6 @@ import getpass
 import click
 
 from bugwarrior.config import get_keyring, load_config
-from bugwarrior.config.parse import ServiceConfig
 from bugwarrior.services import aggregate_issues, get_service
 from bugwarrior.db import (
     get_defined_udas_as_strings,
@@ -61,7 +60,8 @@ def pull(dry_run, flavor, interactive, debug):
         main_section = _get_section_name(flavor)
         config = _try_load_config(main_section, interactive)
 
-        lockfile_path = os.path.join(config.data.path, 'bugwarrior.lockfile')
+        lockfile_path = os.path.join(
+            config[main_section].data.path, 'bugwarrior.lockfile')
         lockfile = PIDLockFile(lockfile_path)
         lockfile.acquire(timeout=10)
         try:
@@ -95,21 +95,13 @@ def vault():
 
 
 def targets():
-    config = load_config('general')
-    for section in config.sections():
-        if section in ['general', 'notifications'] or \
-           section.startswith('flavor.'):
-            continue
-        service_name = config.get(section, 'service')
-        service_class = get_service(service_name)
-        for option in config.options(section):
-            value = config.get(section, option)
-            if not value:
-                continue
+    config = _try_load_config('general')
+    for target in config['general'].targets:
+        service_class = get_service(config[target].service)
+        for value in [v for v in dict(config[target]).values()
+                      if isinstance(v, str)]:
             if '@oracle:use_keyring' in value:
-                service_config = ServiceConfig(
-                    service_class.CONFIG_PREFIX, config, section)
-                yield service_class.get_keyring_service(service_config)
+                yield service_class.get_keyring_service(config[target])
 
 
 @vault.command()

bugwarrior/config/__init__.py
@@ -1,8 +1,4 @@
 from .load import BUGWARRIORRC, load_config
-from .parse import (asbool,
-                    asint,
-                    aslist,
-                    get_taskrc_path)
 from .schema import (ConfigList,
                      ExpandedPath,
                      NoSchemeUrl,
@@ -10,16 +6,10 @@ from .schema import (ConfigList,
                      StrippedTrailingSlashUrl)
 from .secrets import get_keyring, get_service_password
 
-
 __all__ = [
     # load
     'BUGWARRIORRC',
     'load_config',
-    # parse
-    'asbool',
-    'asint',
-    'aslist',
-    'get_taskrc_path',
     # schema
     'ConfigList',
     'ExpandedPath',

bugwarrior/config/data.py
@@ -5,18 +5,7 @@ import subprocess
 from lockfile.pidlockfile import PIDLockFile
 
 
-def get_taskrc_path(conf, main_section):
-    path = os.getenv('TASKRC', '~/.taskrc')
-    if conf.has_option(main_section, 'taskrc'):
-        path = conf.get(main_section, 'taskrc')
-    return os.path.normpath(
-        os.path.expanduser(path)
-    )
-
-
-def get_data_path(config, main_section):
-    taskrc = get_taskrc_path(config, main_section)
-
+def get_data_path(taskrc):
     # We cannot use the taskw module here because it doesn't really support
     # the `_` subcommands properly (`rc:` can't be used for them).
     line_prefix = 'data.location='

bugwarrior/config/load.py
@@ -11,6 +11,21 @@ from . import data, schema
 BUGWARRIORRC = "BUGWARRIORRC"
 
 
+def configure_logging(logfile, loglevel):
+    logging.basicConfig(filename=logfile, level=loglevel)
+
+    # In general, its nice to log "everything", but some of the loggers from
+    # our dependencies are very very spammy.  Here, we silence most of their
+    # noise:
+    spammers = [
+        'bugzilla.base',
+        'bugzilla.bug',
+        'requests.packages.urllib3.connectionpool',
+    ]
+    for spammer in spammers:
+        logging.getLogger(spammer).setLevel(logging.WARN)
+
+
 def get_config_path():
     """
     Determine the path to the config file. This will return, in this order of
@@ -46,51 +61,17 @@ def remove_inactive_flavors(rawconfig, main_section):
             rawconfig.remove_section(section)
 
 
-def fix_logging_path(config, main_section):
-    """
-    Expand environment variables and user home (~) in the log.file and return
-    as relative path.
-    """
-    log_file = config.get(main_section, 'log.file')
-    if log_file:
-        log_file = os.path.expanduser(os.path.expandvars(log_file))
-        if os.path.isabs(log_file):
-            log_file = os.path.relpath(log_file)
-    return log_file
-
-
-def configure_logging(config, main_section):
-    logging.basicConfig(
-        level=getattr(logging, config.get(main_section, 'log.level')),
-        filename=config.get(main_section, 'log.file'),
-    )
-
-    # In general, its nice to log "everything", but some of the loggers from
-    # our dependencies are very very spammy.  Here, we silence most of their
-    # noise:
-    spammers = [
-        'bugzilla.base',
-        'bugzilla.bug',
-        'requests.packages.urllib3.connectionpool',
-    ]
-    for spammer in spammers:
-        logging.getLogger(spammer).setLevel(logging.WARN)
-
-
-def load_config(main_section, interactive=False):
-    config = BugwarriorConfigParser()
-    path = get_config_path()
-    config.readfp(codecs.open(path, "r", "utf-8",))
-    config.set(main_section, 'log.level', 'INFO')
-    config.set(main_section, 'log.file', None)
-    remove_inactive_flavors(config, main_section)
-    config.interactive = interactive
-    config.data = data.BugwarriorData(
-        data.get_data_path(config, main_section))
-    config.set(
-        main_section, 'log.file', fix_logging_path(config, main_section))
-    schema.validate_config(config, main_section, path)
-    configure_logging(config, main_section)
+def load_config(main_section, interactive):
+    configpath = get_config_path()
+    rawconfig = BugwarriorConfigParser()
+    rawconfig.readfp(codecs.open(configpath, "r", "utf-8",))
+    remove_inactive_flavors(rawconfig, main_section)
+    config = schema.validate_config(rawconfig, main_section, configpath)
+    main_config = config[main_section]
+    main_config.interactive = str(interactive)
+    main_config.data = data.BugwarriorData(
+        data.get_data_path(config[main_section].taskrc))
+    configure_logging(main_config.log__file, main_config.log__level)
     return config
 
 

bugwarrior/config/parse.py
@@ -1,68 +1,0 @@
-import configparser
-import logging
-import os
-import re
-import six
-import sys
-
-log = logging.getLogger(__name__)
-
-
-def asbool(some_value):
-    """ Cast config values to boolean. """
-    return six.text_type(some_value).lower() in [
-        'y', 'yes', 't', 'true', '1', 'on'
-    ]
-
-
-def aslist(value):
-    """ Cast config values to lists of strings """
-    return [item.strip() for item in re.split(",(?![^{]*})", value.strip())]
-
-
-def asint(value):
-    """ Cast config values to int, or None for empty strings."""
-    if value == '':
-        return None
-    return int(value)
-
-
-def get_taskrc_path(conf, main_section):
-    path = os.getenv('TASKRC', '~/.taskrc')
-    if conf.has_option(main_section, 'taskrc'):
-        path = conf.get(main_section, 'taskrc')
-    return os.path.normpath(
-        os.path.expanduser(path)
-    )
-
-
-class ServiceConfig(object):
-    """ A service-aware wrapper for ConfigParser objects. """
-    def __init__(self, config_prefix, config_parser, service_target):
-        self.config_prefix = config_prefix
-        self.config_parser = config_parser
-        self.service_target = service_target
-
-    def __getattr__(self, name):
-        """ Proxy undefined attributes/methods to ConfigParser object. """
-        return getattr(self.config_parser, name)
-
-    def __contains__(self, key):
-        """ Does service section specify this option? """
-        return self.config_parser.has_option(
-            self.service_target, self._get_key(key))
-
-    def get(self, key, default=None, to_type=None):
-        try:
-            value = self.config_parser.get(
-                self.service_target, self._get_key(key))
-            if to_type:
-                return to_type(value)
-            return value
-        except configparser.NoSectionError:
-            return default
-        except configparser.NoOptionError:
-            return default
-
-    def _get_key(self, key):
-        return '%s.%s' % (self.config_prefix, key)

bugwarrior/notifications.py
@@ -1,11 +1,8 @@
 from future import standard_library
 standard_library.install_aliases()
-import datetime
 import os
 import urllib.request, urllib.parse, urllib.error
 import warnings
-
-from bugwarrior.config.parse import asbool
 
 
 cache_dir = os.path.expanduser(os.getenv('XDG_CACHE_HOME', "~/.cache") + "/bugwarrior")
@@ -51,7 +48,7 @@ def _get_metadata(issue):
 
 
 def send_notification(issue, op, conf):
-    notify_backend = conf.get('notifications', 'backend')
+    notify_backend = conf.backend
 
     if notify_backend == 'pynotify':
         warnings.warn("pynotify is deprecated.  Use backend=gobject.  "
@@ -73,8 +70,7 @@ def send_notification(issue, op, conf):
                 title="Bugwarrior",
                 description="Finished querying for new issues.\n%s" %
                 issue['description'],
-                sticky=asbool(conf.get(
-                    'notifications', 'finished_querying_sticky', 'True')),
+                sticky=conf.finished_querying_sticky,
                 icon="https://upload.wikimedia.org/wikipedia/"
                 "en/5/59/Taskwarrior_logo.png",
                 priority=1,
@@ -88,8 +84,7 @@ def send_notification(issue, op, conf):
             noteType="New Messages",
             title="Bugwarrior",
             description=message,
-            sticky=asbool(conf.get(
-                'notifications', 'task_crud_sticky', 'True')),
+            sticky=conf.task_crud_sticky,
             icon="https://upload.wikimedia.org/wikipedia/"
             "en/5/59/Taskwarrior_logo.png",
             priority=1,

bugwarrior/services/__init__.py
@@ -16,7 +16,6 @@ import six
 
 from taskw.task import Task
 
-from bugwarrior.config.parse import asbool, asint, aslist, ServiceConfig
 from bugwarrior.config.secrets import get_service_password
 from bugwarrior.db import MARKUP, URLShortener
 
@@ -46,43 +45,15 @@ class IssueService(abc.ABC):
     """ Abstract base class for each service """
     # Which class should this service instantiate for holding these issues?
     ISSUE_CLASS = None
-    # What prefix should we use for this service's configuration values
-    CONFIG_PREFIX = ''
     # Which class defines this service's configuration options?
     CONFIG_SCHEMA = None
 
-    def __init__(self, main_config, main_section, target):
-        self.config = ServiceConfig(self.CONFIG_PREFIX, main_config, target)
-        self.main_section = main_section
+    def __init__(self, config, main_config, target):
+        self.config = config
         self.main_config = main_config
         self.target = target
 
-        self.desc_len = self._get_config_or_default('description_length', 35, asint);
-        self.anno_len = self._get_config_or_default('annotation_length', 45, asint);
-        self.inline_links = self._get_config_or_default('inline_links', True, asbool);
-        self.annotation_links = self._get_config_or_default('annotation_links', not self.inline_links, asbool)
-        self.annotation_comments = self._get_config_or_default('annotation_comments', True, asbool)
-        self.annotation_newlines = self._get_config_or_default('annotation_newlines', False, asbool)
-        self.shorten = self._get_config_or_default('shorten', False, asbool)
-
-        self.default_priority = self.config.get('default_priority', 'M')
-
-        self.add_tags = []
-        for raw_option in aslist(self.config.get('add_tags', '')):
-            option = raw_option.strip(' +;')
-            if option:
-                self.add_tags.append(option)
-
         log.info("Working on [%s]", self.target)
-
-
-    def _get_config_or_default(self, key, default, as_type=lambda x: x):
-        """Return a main config value, or default if it does not exist."""
-
-        if self.main_config.has_option(self.main_section, key):
-            return as_type(self.main_config.get(self.main_section, key))
-        return default
-
 
     def get_templates(self):
         """ Get any defined templates for configuration values.
@@ -113,18 +84,18 @@ class IssueService(abc.ABC):
         """
         templates = {}
         for key in six.iterkeys(Task.FIELDS):
-            template_key = '%s_template' % key
-            if template_key in self.config:
-                templates[key] = self.config.get(template_key)
+            template = getattr(self.config, f'{key}_template')
+            if template:
+                templates[key] = template
         return templates
 
     def get_password(self, key, login='nousername'):
-        password = self.config.get(key)
+        password = getattr(self.config, key)
         keyring_service = self.get_keyring_service(self.config)
         if not password or password.startswith("@oracle:"):
             password = get_service_password(
                 keyring_service, login, oracle=password,
-                interactive=self.config.interactive)
+                interactive=self.main_config.interactive)
         return password
 
     def get_service_metadata(self):
@@ -132,56 +103,50 @@ class IssueService(abc.ABC):
 
     def get_issue_for_record(self, record, extra=None):
         origin = {
-            'annotation_length': self.anno_len,
-            'default_priority': self.default_priority,
-            'description_length': self.desc_len,
+            'annotation_length': self.main_config.annotation_length,
+            'default_priority': self.config.default_priority,
+            'description_length': self.main_config.description_length,
             'templates': self.get_templates(),
             'target': self.target,
-            'shorten': self.shorten,
-            'inline_links': self.inline_links,
-            'add_tags': self.add_tags,
+            'shorten': self.main_config.shorten,
+            'inline_links': self.main_config.inline_links,
+            'add_tags': self.config.add_tags,
         }
         origin.update(self.get_service_metadata())
         return self.ISSUE_CLASS(record, origin=origin, extra=extra)
 
     def build_annotations(self, annotations, url):
         final = []
-        if self.annotation_links:
+        if self.main_config.annotation_links:
             final.append(url)
-        if self.annotation_comments:
+        if self.main_config.annotation_comments:
             for author, message in annotations:
                 message = message.strip()
                 if not message or not author:
                     continue
 
-                if not self.annotation_newlines:
+                if not self.main_config.annotation_newlines:
                     message = message.replace('\n', '').replace('\r', '')
 
-                if self.anno_len:
+                annotation_length = self.main_config.annotation_length
+                if annotation_length:
                     message = '%s%s' % (
-                        message[:self.anno_len],
-                        '...' if len(message) > self.anno_len else ''
+                        message[:annotation_length],
+                        '...' if len(message) > annotation_length else ''
                     )
                 final.append('@%s - %s' % (author, message))
         return final
 
     def include(self, issue):
         """ Return true if the issue in question should be included """
-        only_if_assigned = self.config.get('only_if_assigned', None)
-
-        if only_if_assigned:
+        if self.config.only_if_assigned:
             owner = self.get_owner(issue)
-            include_owners = [only_if_assigned]
+            include_owners = [self.config.only_if_assigned]
 
-            if self.config.get('also_unassigned', None, asbool):
+            if self.config.also_unassigned:
                 include_owners.append(None)
 
             return owner in include_owners
-
-        only_if_author = self.config.get('only_if_author', None)
-
-        if only_if_author:
-            return self.get_author(issue) == only_if_author
 
         return True
 
@@ -475,7 +440,7 @@ class ServiceClient:
             return response.json
 
 
-def _aggregate_issues(conf, main_section, target, queue, service_name):
+def _aggregate_issues(conf, main_section, target, queue):
     """ This worker function is separated out from the main
     :func:`aggregate_issues` func only so that we can use multiprocessing
     on it for speed reasons.
@@ -484,7 +449,8 @@ def _aggregate_issues(conf, main_section, target, queue, service_name):
     start = time.time()
 
     try:
-        service = get_service(service_name)(conf, main_section, target)
+        service = get_service(conf[target].service)(
+            conf[target], conf[main_section], target)
         issue_count = 0
         for issue in service.issues():
             queue.put(issue)
@@ -513,7 +479,7 @@ def aggregate_issues(conf, main_section, debug):
     log.info("Starting to aggregate remote issues.")
 
     # Create and call service objects for every target in the config
-    targets = aslist(conf.get(main_section, 'targets'))
+    targets = conf[main_section].targets
 
     queue = multiprocessing.Queue()
 
@@ -521,18 +487,12 @@ def aggregate_issues(conf, main_section, debug):
 
     if debug:
         for target in targets:
-            _aggregate_issues(
-                conf,
-                main_section,
-                target,
-                queue,
-                conf.get(target, 'service')
-            )
+            _aggregate_issues(conf, main_section, target, queue)
     else:
         for target in targets:
             proc = multiprocessing.Process(
                 target=_aggregate_issues,
-                args=(conf, main_section, target, queue, conf.get(target, 'service'))
+                args=(conf, main_section, target, queue)
             )
             proc.start()
 

commit 201fd1192838607c79b184f620f4ce47697298e6

Fix traceback when enabling notifications
Commit 1cbbcb6 introduced a regression that causes a traceback when
enabling notifications.

Fix #864

bugwarrior/db.py
@@ -495,7 +495,7 @@ def synchronize(issue_generator, conf, main_section, dry_run=False):
         updates = (len(issue_updates['new']) +
                    len(issue_updates['changed']) +
                    len(issue_updates['closed']))
-        if not main_config.notifications.only_on_new_tasks or updates > 0:
+        if not conf['notifications'].only_on_new_tasks or updates > 0:
             send_notification(
                 dict(
                     description="New: %d, Changed: %d, Completed: %d" % (