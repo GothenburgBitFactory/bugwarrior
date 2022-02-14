00dc39761c5d74c26a6c6844db20bd3ceac698e2
ryneeverett <ryneeverett@gmail.com>
2022-02-13 20:03:03 -0500

    Test that example configuration is valid.
    The only reason it already is valid is that I'd manually validated it
    and fixed the bugs as part of #848. This will ensure we don't introduce
    errors going forward.
    
    In order to test it I moved it into a separate file which I pull in
    via an include directive, which was a good opportunity to add syntax
    highlighting.

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,11 +1,26 @@
+import os.path
+import pathlib
 import subprocess
 import tempfile
 import unittest
+
+from bugwarrior import config
+
+from .base import ConfigTest
+
+DOCS_PATH = str(pathlib.Path(__file__).parent / '../bugwarrior/docs')
 
 
 class DocsTest(unittest.TestCase):
     def test_docs_build_without_warning(self):
         with tempfile.TemporaryDirectory() as buildDir:
             subprocess.run(
-                ['sphinx-build', '-n', '-W', 'bugwarrior/docs', buildDir],
+                ['sphinx-build', '-n', '-W', DOCS_PATH, buildDir],
                 check=True)
+
+
+class ExampleBugwarriorrcTest(ConfigTest):
+    def test_example_bugwarriorrc(self):
+        os.environ['BUGWARRIORRC'] = os.path.join(
+            DOCS_PATH, 'example-bugwarriorrc')
+        config.load_config('general', False)
