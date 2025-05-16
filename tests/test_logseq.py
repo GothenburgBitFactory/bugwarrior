02bd5d8b42ddc62ae1e51a0f828aa7872631fe67
Stephen Cross <scross01@gmail.com>
2025-05-16 18:12:40 -0400

    Update testcase

--- tests/test_logseq.py
+++ tests/test_logseq.py
@@ -33,7 +33,7 @@ class TestLogseqIssue(AbstractServiceTest, ServiceTest):
             {"id": 1777},
             {"id": 7070},
         ],
-        "content": ("DOING [#C] Do something #[[Test tag]] #[[TestTag]] #TestTag\n"
+        "content": ("DOING [#C] Do something #[[Test tag one]] #[[TestTagTwo]] #TestTagThree\n"
                     "id:: 67dae9ea-8e4d-4ad1-91dc-72aacc72a802\n"
                     ":LOGBOOK:\n"
                     "CLOCK: [2025-06-03 Tue 13:56:47]--[2025-06-03 Tue 13:56:49] =>  00:00:02\n"
@@ -74,11 +74,11 @@ class TestLogseqIssue(AbstractServiceTest, ServiceTest):
             "status": "pending",
             "priority": "L",
             "project": self.test_extra["graph"],
-            "tags": ['TestTag', 'Testtag', 'TestTag'],
+            "tags": ['Testtagone', 'TestTagTwo', 'TestTagThree'],
             issue.ID: int(self.test_record["id"]),
             issue.UUID: self.test_record["uuid"],
             issue.STATE: self.test_record["marker"],
-            issue.TITLE: "Do something #【Test tag】 #【TestTag】 #TestTag",
+            issue.TITLE: "Do something #【Test tag one】 #【TestTagTwo】 #TestTagThree",
             issue.URI: self.test_extra["baseURI"] + self.test_record["uuid"],
         }
 
@@ -94,7 +94,7 @@ class TestLogseqIssue(AbstractServiceTest, ServiceTest):
         expected = {
             "annotations": [],
             "description": f"(bw)Is#{self.test_record['id']}"
-            + " - Do something #【Test tag】 #【TestTag】 #TestTag"
+            + " - Do something #【Test tag one】 #【TestTagTwo】 #TestTagThree"
             + " .. " + self.test_extra["baseURI"] + self.test_record["uuid"],
             "due": None,
             "scheduled": None,
@@ -102,11 +102,11 @@ class TestLogseqIssue(AbstractServiceTest, ServiceTest):
             "status": "pending",
             "priority": "L",
             "project": self.test_extra["graph"],
-            "tags": ['TestTag', 'Testtag', 'TestTag'],
+            "tags": ['Testtagone', 'TestTagTwo', 'TestTagThree'],
             issue.ID: int(self.test_record["id"]),
             issue.UUID: self.test_record["uuid"],
             issue.STATE: self.test_record["marker"],
-            issue.TITLE: "Do something #【Test tag】 #【TestTag】 #TestTag",
+            issue.TITLE: "Do something #【Test tag one】 #【TestTagTwo】 #TestTagThree",
             issue.URI: self.test_extra["baseURI"] + self.test_record["uuid"],
         }
 
