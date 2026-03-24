adb47ee9dd47172aeef15294d5be75956123a612
Laurent Tramoy <laurenttramoy@gmail.com>
2026-03-24 11:35:32 +0100

    fix: add back ruff and ty to tests

---
+++ tests/test_general.py
@@ -0,0 +1,13 @@
+import subprocess
+import unittest
+
+
+class TestGeneral(unittest.TestCase):
+    def test_ruff_check(self):
+        subprocess.run(['ruff', 'check'], check=True)
+
+    def test_ruff_format(self):
+        subprocess.run(['ruff', 'format', '--check'], check=True)
+
+    def test_ty_check(self):
+        subprocess.run(['ty', 'check'], check=True)
