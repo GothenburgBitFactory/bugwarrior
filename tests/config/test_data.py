8807a2981134f353bd78940fb314fff3361b66a2
Laurent Tramoy <laurenttramoy@gmail.com>
2026-07-09 10:11:22 +0200

    Convert config tests to fixtures

--- tests/config/test_data.py
+++ tests/config/test_data.py
@@ -1,72 +1,74 @@
 import json
 import os
+from pathlib import Path
+
+import pytest
 
 from bugwarrior.config import data, schema
 
-from ..base import ConfigTest
+
+def assert_0600(bw_data):
+    permissions = oct(os.stat(bw_data._datafile).st_mode & 0o777)
+    # python2 -> 0600, python3 -> 0o600
+    assert permissions in ['0600', '0o600']
 
 
-class TestData(ConfigTest):
-    def setUp(self):
-        super().setUp()
-        self.data = data.BugwarriorData(self.lists_path)
+class TestData:
+    @pytest.fixture
+    def bw_data(self, config_environment):
+        return data.BugwarriorData(config_environment.lists_path)
 
-    def assert0600(self):
-        permissions = oct(os.stat(self.data._datafile).st_mode & 0o777)
-        # python2 -> 0600, python3 -> 0o600
-        assert permissions in ['0600', '0o600']
-
-    def test_get_set(self):
+    def test_get_set(self, bw_data):
         # "touch" data file.
-        with open(self.data._datafile, 'w+') as handle:
+        with open(bw_data._datafile, 'w+') as handle:
             json.dump({'old': 'stuff'}, handle)
 
-        self.data.set('key', 'value')
+        bw_data.set('key', 'value')
 
-        assert self.data.get('key') == 'value'
-        assert self.data.get_data() == {'old': 'stuff', 'key': 'value'}
-        self.assert0600()
+        assert bw_data.get('key') == 'value'
+        assert bw_data.get_data() == {'old': 'stuff', 'key': 'value'}
+        assert_0600(bw_data)
 
-    def test_set_first_time(self):
-        self.data.set('key', 'value')
+    def test_set_first_time(self, bw_data):
+        bw_data.set('key', 'value')
 
-        assert self.data.get('key') == 'value'
-        self.assert0600()
+        assert bw_data.get('key') == 'value'
+        assert_0600(bw_data)
 
-    def test_path_attribute(self):
-        assert self.data.path == self.lists_path
+    def test_path_attribute(self, bw_data, config_environment):
+        assert bw_data.path == config_environment.lists_path
 
 
-class TestGetDataPath(ConfigTest):
-    def setUp(self):
-        super().setUp()
-        self.main_config = schema.MainSectionConfig(targets=[])
+class TestGetDataPath:
+    @pytest.fixture
+    def main_config(self):
+        return schema.MainSectionConfig(targets=[])
 
-    def assertDataPath(self, expected_datapath):
-        assert expected_datapath == data.get_data_path(self.main_config.taskrc)
+    def assert_data_path(self, main_config, expected_datapath):
+        assert str(expected_datapath) == data.get_data_path(main_config.taskrc)
 
-    def test_TASKDATA(self):
+    def test_TASKDATA(self, main_config, monkeypatch, tmp_path):
         """
         TASKDATA should be respected, even when taskrc's data.location is set.
         """
-        datapath = os.environ['TASKDATA'] = os.path.join(self.tempdir, 'data')
-        self.assertDataPath(datapath)
+        datapath = tmp_path / 'data'
+        monkeypatch.setenv('TASKDATA', str(datapath))
+        self.assert_data_path(main_config, datapath)
 
-    def test_taskrc_datalocation(self):
+    def test_taskrc_datalocation(self, main_config, config_environment):
         """
         When TASKDATA is not set, data.location in taskrc should be respected.
         """
         assert 'TASKDATA' not in os.environ
-        self.assertDataPath(self.lists_path)
+        self.assert_data_path(main_config, config_environment.lists_path)
 
-    def test_unassigned(self):
+    def test_unassigned(self, main_config, config_environment):
         """
         When data path is not assigned, use default location.
         """
         # Empty taskrc.
-        with open(self.taskrc, 'w'):
-            pass
+        config_environment.taskrc.write_text('')
 
         assert 'TASKDATA' not in os.environ
 
-        self.assertDataPath(os.path.expanduser('~/.task'))
+        self.assert_data_path(main_config, Path.home() / '.task')
